625ec09ba25e13a52a4b23456bc018c2d563ce80
Maeva NAP <maevanap.contact@gmail.com>
2018-01-22 10:56:03 +0100

    add JS code to change workspace

---
+++ main.js
@@ -0,0 +1,67 @@
+/* Operations when the DOM is ready */
+$(function() {
+  /* Variables DOM */
+  const WORKSPACE_SELECT = $('#workspaces-select');
+
+  $(WORKSPACE_SELECT).change(function() {
+    changeWorkspace();
+  });
+
+  function changeWorkspace() {
+    let elementSelected = $('#workspaces-select option:selected');
+
+    const TOOLS_BAR = $('#tools-bar');
+    const MAIN_TOOLS = $('#main-tools');
+    const TOOLS_BAR_ITEM_1 = $('#tools-bar-item-1');
+    const TOOLS_BAR_ITEM_2 = $('#tools-bar-item-2');
+    const TOOLS_BAR_ITEM_3 = $('#tools-bar-item-3');
+    const ZOOM_FORM = $('#zoom-form');
+    const TOOLS_BAR_INSIDE = $('#tools-bar-inside');
+    const FA_PLUS_TOOL = $('#tools-bar-item-1 .fa-plus');
+    const FA_MINUS_TOOL = $('#tools-bar-item-1 .fa-minus');
+    const ITEMS_ESSENTIALS_WS = $('#items-essential-ws');
+    const MAIN_TITLE = $('#main-title');
+    const ICON_CLICK_UNDO = $('#icon-click-undo');
+    const ICON_CLICK_REDO = $('#icon-click-redo');
+
+    /* Reset the workspace */
+    TOOLS_BAR.removeClass('left-ws');
+    MAIN_TOOLS.removeClass('left-ws');
+    TOOLS_BAR_ITEM_1.removeClass('left-ws');
+    TOOLS_BAR_ITEM_2.removeClass('left-ws');
+    TOOLS_BAR_ITEM_3.removeClass('left-ws');
+    ZOOM_FORM.removeClass('zoom-form-left-ws');
+    TOOLS_BAR_INSIDE.removeClass('left-ws');
+    FA_PLUS_TOOL.removeClass('hidden');
+    FA_MINUS_TOOL.removeClass('hidden');
+    ITEMS_ESSENTIALS_WS.addClass('hidden');
+    MAIN_TITLE.removeClass('hidden');
+    ICON_CLICK_UNDO.removeClass('hidden');
+    ICON_CLICK_REDO.removeClass('hidden');
+    TOOLS_BAR.removeClass('hidden');
+
+    switch(elementSelected.attr('value')) {
+      case 'default':
+        
+      break;
+      case 'left':
+        TOOLS_BAR.addClass('left-ws');
+        MAIN_TOOLS.addClass('left-ws');
+        TOOLS_BAR_ITEM_1.addClass('left-ws');
+        TOOLS_BAR_ITEM_2.addClass('left-ws');
+        TOOLS_BAR_ITEM_3.addClass('left-ws');
+        ZOOM_FORM.addClass('zoom-form-left-ws');
+        TOOLS_BAR_INSIDE.addClass('left-ws');
+        FA_PLUS_TOOL.addClass('hidden');
+        FA_MINUS_TOOL.addClass('hidden');
+      break;
+      case 'essentials':
+        ITEMS_ESSENTIALS_WS.removeClass('hidden');
+        MAIN_TITLE.addClass('hidden');
+        ICON_CLICK_UNDO.addClass('hidden');
+        ICON_CLICK_REDO.addClass('hidden');
+        TOOLS_BAR.addClass('hidden');
+      break;
+    }
+  }
+});
